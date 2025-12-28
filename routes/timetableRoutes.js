1115edc50532d89f7d19ef0c3280bdca6040e929
amritiano <156211338+amritiano@users.noreply.github.com>
2025-12-28 16:34:15 +0530

    moved patch above exportsit timetable route

--- routes/timetableRoutes.js
+++ routes/timetableRoutes.js
@@ -10,9 +10,11 @@ router.get("/:schoolId/timetables", timetableController.getTimetable);
 // POST timetable
 router.post("/:schoolId/timetables", timetableController.createTimetable);
 
-module.exports = router;
-
 router.patch(
   "/:schoolId/timetables/:id",
   timetableController.updateTimetable
 );
+module.exports = router;
+
+
+
